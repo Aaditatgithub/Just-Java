37306291efeeea83e5f4c79afd45fd6f9c3e8266
Aaditya Patil <131251920+Aaditatgithub@users.noreply.github.com>
2024-03-23 13:48:58 +0530

    Delete Getting-Started directory

--- Getting-Started/CWH_04_LITERALS.java
+++
@@ -1,7 +0,0 @@
-public class CWH_04_LITERALS {
-    public static void main(String[] args){
-        long age = 34L;
-        char ch = 'F';
-        System.out.print(age);
-    }
-}
--- Getting-Started/Learning_strings.java
+++
@@ -1,8 +0,0 @@
-public class Learning_strings {
-    public static void main(String[] args){
-        String str = "Harry";
-        System.out.println(str.toUpperCase());
-
-        //lowercase, uppercase, trim, indexOf, At
-    }
-}
